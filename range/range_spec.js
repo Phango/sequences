c98dc8be9eee6b1197ffa2fcbc2ca7fed56bd817
Michael Pang <michaelpang@Michaels-MacBook-Pro.local>
2017-08-09 13:31:36 +0200

    updated range tests

--- range/range_spec.js
+++ range/range_spec.js
@@ -6,5 +6,6 @@ describe('Range', () => {
   it('byCount() should return an array', () => {
     expect(range.byCount(1,5)).toEqual([1,2,3,4,5]);
     expect(range.byCount(5,5)).toEqual([5,6,7,8,9]);
+    expect(range.byCount(10,10)).toEqual([10,11,12,13,14,15,16,17,18,19]);
   });
 })
